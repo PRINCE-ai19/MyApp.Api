76eb8b3bc48e51e460eecedee6747aef9cf7a86d
LuanPham <luanpham45794@gmail.com>
2026-04-13 16:17:17 +0700

    .

--- MyApp.Api/Controllers/CategoriesController.cs
+++ MyApp.Api/Controllers/CategoriesController.cs
@@ -2,6 +2,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MyApp.Application.Model_DTO;
 using MyApp.Application.Services;
+using MediatR;
+using MyApp.Application.Features.Categories.Queries.GetListCategory;
+using MyApp.Application.Features.Categories.Commands;
+using Microsoft.VisualBasic;
 
 namespace MyApp.Api.Controllers
 {
@@ -10,16 +14,18 @@ namespace MyApp.Api.Controllers
     public class CategoriesController : ControllerBase
     {
         private readonly ICategoryService _categoryService;
+        private readonly ISender _sender;
 
-        public CategoriesController(ICategoryService categoryService)
+        public CategoriesController(ICategoryService categoryService, ISender sender)
         {
             _categoryService = categoryService;
+            _sender = sender;
         }
 
         [HttpGet("lay/category")]
         public async Task<IActionResult> Get()
         {
-            var data = await _categoryService.GetListCategoryForUI();
+            var data = await _sender.Send(new GetListCategoryQuery());
             return Ok(data);
         }
 
@@ -27,37 +33,36 @@ namespace MyApp.Api.Controllers
         public async Task<IActionResult> Create([FromBody] Category_DTO dto)
         {
 
-            var result = await _categoryService.AddCategory(dto);
+            var result = await _sender.Send(new CreateCategoryCommand(dto));
             if (!result.Success)
             {
-                return BadRequest(result);
+                return BadRequest(new { message = result.Message });
             }
-
-            return Ok(result);
+            return Ok(new { message = result.Message });
         }
 
         [HttpPost("sua/category{id}")]
-        public async Task<IActionResult> Update(int id, [FromBody] CategoryUpdateDto dto)
+        public async Task<IActionResult> Update(int id, [FromBody] Category_DTO dto)
         {
 
-            var result = await _categoryService.UpdateCategory(id, dto);
+            var result = await _sender.Send(new UpdateProductCommand(id, dto));
+
             if (!result.Success)
             {
-                return BadRequest(result);
+                return BadRequest(new { message = result.Message });
             }
-
-            return Ok(result);
+            return Ok(new { message = result.Message });
         }
 
         [HttpPost("delete/category/{id}")]
         public async Task<IActionResult> Delete(int id)
         {
-            var result = await _categoryService.DeleteCategory(id);
+            var result = await _sender.Send(new DeleteCategoryCommad(id));
             if (!result.Success)
             {
-                return BadRequest(result);
+                return BadRequest(new { message = result.Message });
             }
-            return Ok(result);
+            return Ok(new { message = result.Message });
         }
     }
 }
